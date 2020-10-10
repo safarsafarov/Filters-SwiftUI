d850c2cf1b93d3012fdf7fab5fcb5128e1422866
safarsafarov <safarov.safar@hotmail.com>
2020-10-10 11:11:10 +0800

    project

---
+++ Filters-SwiftUI/Filters_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Filters_SwiftUIApp.swift
+//  Filters-SwiftUI
+//
+//  Created by Safar Safarov on 2020/10/10.
+//
+
+import SwiftUI
+
+@main
+struct Filters_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
